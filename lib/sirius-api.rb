90dc2c8130b37314d91001bf04f367db6c28d12d
Arda Тetik <ardatetik1881@gmail.com>
2025-07-31 21:37:36 +0300

    Sirius-API

--- lib/sirius-api.rb
+++ lib/sirius-api.rb
@@ -15,229 +15,74 @@ module SiriusApi
     reset_password: URI("https://api.socialsirius.store/api/v1/auth/reset-password"),
   }
 
-  def self.register(email, username, password, first_name, last_name, birthdate)
-    begin
-      # Uzantıları barındıran kümeden ilgili apinin alınması
-      uri = URIS[:register]
-
-      # HTTP nesnesinin tanımlanması
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true # SSL kullanımı aktif edilir
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE # SSL için herhangi bir doğrulama yapılmasın
-
-      # Request tanımlanması ve header verilmesi
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
+  class << self
+    def register(email, username, password, first_name, last_name, birthdate)
+      post(:register, {
+        email: email,
+        username: username,
+        password: password,
+        firstName: first_name,
+        lastName: last_name,
+        birthdate: birthdate
       })
-
-      # Data isimli bir sözlük oluşturulur
-      # Fonksiyondan gelen veriler ile eşleştirilir
-      data = {
-        "email" => email,
-        "username" => username,
-        "password" => password,
-        "firstName" => first_name,
-        "lastName" => last_name,
-        "birthdate" => birthdate
-      }
-
-      # İstek gövdesine veri json olarak eklenir
-      request.body = data.to_json
-
-      # Oluşturulan http nesnesi ile tanımlanan istek gönderilir
-      response = http.request(request)
-
-      # response döndürülür
-      return response
-    rescue Exception => err
-      # hata yakalama bloğu
-      puts "Error: #{err}"
-
-      # hata varsa boş veri döndür
-      return nil
     end
-  end
 
-  def self.refresh_token(token)
-    begin
-      uri = URIS[:refresh_token]
+    def login(identifier, password)
+      post(:login, {
+        identifier: identifier,
+        password: password
+      })
+    end
 
+    def activate_user(token)
+      post(:activate, { token: token })
+    end
+
+    def logout(token)
+      post(:logout, { token: token })
+    end
+
+    def refresh_token(token)
+      post(:refresh_token, { refreshToken: token })
+    end
+
+    def resend_activation(email)
+      post(:resend_activation, { email: email })
+    end
+
+    def forgot_password(email)
+      post(:forgot_password, { email: email })
+    end
+
+    def reset_password(token, password)
+      post(:reset_password, {
+        token: token,
+        newPassword: password
+      })
+    end
+
+    private
+
+    def post(endpoint_key, data)
+      uri = URIS[endpoint_key]
+      http = build_http(uri)
+      request = Net::HTTP::Post.new(uri.path, { "Content-Type" => "application/json" })
+      request.body = data.to_json
+      perform_request(http, request)
+    end
+
+    def build_http(uri)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
-      })
-
-      data = {
-        "refreshToken" => token,
-      }
-
-      request.body = data.to_json
-      response = http.request(request)
-
-      return response
-    rescue Exception => err
-      puts "Error: #{err}"
-      return nil
+      http
     end
-  end
 
-  def self.logout(token)
-    begin
-      uri = URIS[:logout]
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
-      })
-
-      data = {
-        "token" => token,
-      }
-
-      request.body = data.to_json
-      response = http.request(request)
-
-      return response
-    rescue Exception => err
-      puts "Error: #{err}"
-      return nil
-    end
-  end
-
-  def self.resend_activation(email)
-    begin
-      uri = URIS[:resend_activation]
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
-      })
-
-      data = {
-        "email" => email,
-      }
-
-      request.body = data.to_json
-      response = http.request(request)
-
-      return response
-    rescue Exception => err
-      puts "Error: #{err}"
-      return nil
-    end
-  end
-
-  def self.forgot_password(email)
-    begin
-      uri = URIS[:forgot_password]
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
-      })
-
-      data = {
-        "email" => email,
-      }
-
-      request.body = data.to_json
-      response = http.request(request)
-
-      return response
-    rescue Exception => err
-      puts "Error: #{err}"
-      return nil
-    end
-  end
-
-  def self.reset_password(token, password)
-    begin
-      uri = URIS[:reset_password]
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
-      })
-
-      data = {
-        "token" => token,
-        "newPassword" => password,
-      }
-
-      request.body = data.to_json
-      response = http.request(request)
-
-      return response
-    rescue Exception => err
-      puts "Error: #{err}"
-      return nil
-    end
-  end
-  def self.activate_user(token)
-    begin
-      uri = URIS[:activate]
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
-      })
-
-      data = {
-        "token" => token,
-      }
-
-      request.body = data.to_json
-      response = http.request(request)
-
-      return response
-    rescue Exception => err
-      puts "Error: #{err}"
-      return nil
-    end
-  end
-
-  def self.login(identifier, password)
-    begin
-      uri = URIS[:login]
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(uri.path, {
-        "Content-Type" => "application/json"
-      })
-
-      data = {
-        "identifier" => identifier,
-        "password" => password
-      }
-
-      request.body = data.to_json
-      response = http.request(request)
-
-      return response
-    rescue Exception => err
-      puts "Error: #{err}"
-      return nil
+    def perform_request(http, request)
+      http.request(request)
+    rescue StandardError => e
+      warn "[SiriusApi Error] #{e.class}: #{e.message}"
+      nil
     end
   end
 end
